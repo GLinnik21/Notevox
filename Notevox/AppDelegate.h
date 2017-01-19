d4b5bbb453be2e86d119ad84d941b86b7a9f595e
Gleb Linnik <gleb_linnik@icloud.com>
2017-01-19 16:32:36 +0300

    Initial Commit

---
+++ Notevox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notevox
+//
+//  Created by Глеб Линник on 19.01.17.
+//  Copyright © 2017 ARobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
